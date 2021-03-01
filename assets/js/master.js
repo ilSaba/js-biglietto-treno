dbb3fedf32a7a2302b01b7be9e7bb543bdf421cc
Andrea Sabatini <sabatini.andrea@outlook.it>
2021-03-01 15:55:42 +0100

    Chiedo anni + km e calcolo prezzo standard

---
+++ assets/js/master.js
@@ -0,0 +1,10 @@
+var anni = parseInt(prompt('Quanti anni hai?'));
+
+var km = parseFloat(prompt('Quanti kilometri vuoi percorrere?'));
+
+var prezzo = 0.21;
+
+var prezzoBiglietto = km * prezzo;
+
+console.log(prezzoBiglietto);
+
