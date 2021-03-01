3c657ac24e29841ec8388b36eb2574d2e3f52cd0
Andrea Sabatini <sabatini.andrea@outlook.it>
2021-03-01 17:01:43 +0100

    Esercizio completato + css incoming

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,10 +1,17 @@
+// Chiedo età;
 var anni = parseInt(prompt('Quanti anni hai?'));
-
+// Chiedo km;
 var km = parseFloat(prompt('Quanti kilometri vuoi percorrere?'));
-
+// Prezzo biglietto x km;
 var prezzo = 0.21;
-
+// Calcolo prezzo biglietto;
 var prezzoBiglietto = km * prezzo;
 
-console.log(prezzoBiglietto);
-
+// Calcolo sconti per fasce d'età;
+if (anni < 18) {
+    alert("Il prezzo del tuo biglietto è €" + (prezzoBiglietto - ((prezzoBiglietto * 20) /100) + " perchè sei minorenne"));
+  } else if (anni > 65) {
+    alert("Il prezzo del tuo biglietto è €" + (prezzoBiglietto - (prezzoBiglietto * 40) /100 + " perchè sei vecchia"));
+  } else {
+    alert("Il prezzo del tuo biglietto è €" + prezzoBiglietto);
+  }
